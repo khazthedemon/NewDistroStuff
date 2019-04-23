83f704b6206cd1d095e590e1809f35d1f63c44a6
khazthedemon <richardsoyer@gmail.com>
2019-04-23 00:41:50 -0300

    First Commit

---
+++ apt-get-install.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+sudo apt-get install adb fastboot telegram-desktop bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev openjdk-8-jdk openjdk-13-jdk && sudo snap install spotify
